8e1126e0a8e5580729d613021b83f17353582d93
1997godown <1997godown@gmail.com>
2019-03-18 07:45:17 -0500

    Inital Map
    Inital Map

---
+++ Project Newton (2)/Assets/Scripts/LeftRightCloud.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRightCloud : MonoBehaviour
+{
+    float dirX, moveSpeed = 5f;
+    bool moveRight = true;
+    float s;
+    // Update is called once per frame
+
+     void Start()
+    {
+        s = transform.position.x;
+    }
+    void Update()
+    {
+        if (transform.position.x > s+4)//-20f)
+            moveRight = false;
+        if (transform.position.x < s-4) //-35f)
+            moveRight = true;
+
+        if (moveRight)
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+    }
+}
